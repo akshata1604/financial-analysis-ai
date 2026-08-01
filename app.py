747af3edeac7d651119cf030fdbb5a17aea6907c
akshata1604 <akshatakelkar16@gmail.com>
2026-08-01 19:29:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routes.analysis import router
+
+app = FastAPI()
+
+app.include_router(router)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to Financial Analysis AI API!"
+    }
